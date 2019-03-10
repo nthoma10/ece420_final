476e39d0fb211477e239ceb021b551e13f22f931
nthoma10 <natasha@karingada.com>
2019-03-10 16:52:28 -0500

    modified:   hough.py

--- hough.py
+++ hough.py
@@ -1,26 +1,17 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-img = cv2.imread('sudoku.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
+img_rgb = cv2.imread('resistor.jpg')
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+template = cv2.imread('resistor.jpg',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
-lines = cv2.HoughLines(edges,1,np.pi/180,200)
-for i in range(0, len(lines)):
-    for rho, theta in lines[i]:
-        print(rho, theta)
-
-        a = np.cos(theta)
-        b = np.sin(theta)
-
-        x0 = a*rho
-        y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
-
-        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-
-cv2.imshow("image", img)
+cv2.imshow("image", img_rgb)
 cv2.waitKey()
+
