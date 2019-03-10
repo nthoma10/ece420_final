77710a6806eaca7c3faeb65edf5b543c065f43b2
nthoma10 <natasha@karingada.com>
2019-03-10 15:53:00 -0500

    modified:   hough.py

--- hough.py
+++ hough.py
@@ -5,20 +5,25 @@ img = cv2.imread('sudoku.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
-lines = cv2.HoughLines(edges,1,np.pi/180,100)
-print(lines)
-#for rho,theta in lines:
-    #print(rho, theta)
-    #a = np.cos(theta)
-    #b = np.sin(theta)
-    #x0 = a*rho
-    #y0 = b*rho
-    #x1 = int(x0 + 1000*(-b))
-    #y1 = int(y0 + 1000*(a))
-    #x2 = int(x0 - 1000*(-b))
-    #y2 = int(y0 - 1000*(a))
+lines = cv2.HoughLines(edges,1,np.pi/180,200)
+for rho, theta in lines[0]:
+    print(lines[0])
+    print(rho, theta)
+for i in range(0, len(lines)):
+    for rho, theta in lines[i]:
+        print(rho, theta)
 
-    #cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+        a = np.cos(theta)
+        b = np.sin(theta)
+
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
 cv2.imshow("image", img)
 cv2.waitKey()
