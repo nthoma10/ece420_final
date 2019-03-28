865cf99ac882c63ad399db5a2322d7a83fef745d
nthoma10 <natasha@karingada.com>
2019-03-27 20:03:19 -0500

    new file:   420_notebook.ipynb 	modified:   hough.py

--- hough.py
+++ hough.py
@@ -1,24 +1,7 @@
 import cv2
 import numpy as np
 import random
-from matplotlib import pyplot as plt
 
-'''
-img_rgb = cv2.imread('circuit.jpg')
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('resistor.jpg',0)
-w, h = template.shape[::-1]
-
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-print(res)
-threshold = -0.3
-loc = np.where(res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-
-cv2.imshow("image", img_rgb)
-cv2.waitKey()
-'''
 
 img = cv2.imread("circuit.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -31,8 +14,9 @@ res = cv2.matchTemplate(gray, template, cv2.TM_SQDIFF_NORMED)
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
 # create threshold from min val, find where sqdiff is less than thresh
+print(min_val,max_val)
 match_locations = np.where(res>=0.23)
-#print(match_locations)
+print(len(match_locations[0]))
 # draw template match boxes
 w, h = template.shape[::-1]
 for (x, y) in zip(match_locations[1], match_locations[0]):
